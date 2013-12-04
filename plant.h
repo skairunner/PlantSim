33a5342686c53a1ba5fcbc588962f97abc49475f
Skyunner <fluffyterran@gmail.com>
2013-12-05 00:04:25 +0900

    added floral induction.

--- plant.h
+++ plant.h
@@ -2,30 +2,25 @@
 #include "enums.h"
 #include <map>
 #include "Weather.h"
-
+#include "curves.h"
 
 
 namespace ALMANAC
   {
   class SoilCell;
-  /**
-  This class stores the three parameters for S-curves.
-  An S-curve is in the form of y = v / (1 + exp(-s * (x - h);
-  _v_ert determines the max value of the curve, or what number it approaches when x->oo
-  _s_cale determines how stretched out the curve is.
-  _h_orizontal determines where the 'center' of the curve is.
-  **/
-  struct SCurveNumbers
-    {
-    SCurveNumbers(const double& vertical, const double& Scale, const double& horizontal);
-    SCurveNumbers();
-    double scale, horiz, vert;
+  
 
-    static double getS_CurveNum(const SCurveNumbers& scv, const double& x)
-      {
-      return scv.vert / (1 + exp(-scv.scale * (x - scv.horiz)));
-      }
-    };
+  struct BiomassHolder
+  {
+      BiomassHolder();
+      BiomassHolder(double Stem, double Roots, double Storage, double Fruits);
+      double stem;
+      double roots;
+      double storageOrgan;
+      double flowerAndfruits;
+      double getBiomass() const;
+      operator double() const;
+  };
 
 
 
@@ -33,13 +28,14 @@ namespace ALMANAC
     {
     public:
       BasePlant(SoilCell* soil = 0);
-      void calculate(const WeatherData& data, const double& albedo); // plug in today's weather :v. CO2 is in ppm
+      void calculate(const WeatherData& data, const double& albedo, const double radiation = -1); // plug in today's weather :v. CO2 is in ppm
       void findREG(); // probably has params
       double getHU(); // heat units
       double findHUI(); // heat unit indx, basically % grown.
       double calcHeight();
       double calcRootDepth();
       double getBiomass();
+      BiomassHolder getBiomassStruct();
       double getLAI();
       double getRequiredWater();
 
@@ -47,6 +43,7 @@ namespace ALMANAC
       double getREG();
 
       double getNitrogen();
+      bool flowering();
 
     private:
       std::map<int, double> growthStages;
@@ -67,13 +64,26 @@ namespace ALMANAC
       double rootFraction1, rootFraction2; // fraction of root weight; 1 is at germination, 2 at maturity
       double maxRootDepth; //mm     
 
-      SCurveNumbers HeatUnitFactorNums;
-      SCurveNumbers CO2CurveFactors;
+      SCurve HeatUnitFactorNums;
+      SCurve CO2CurveFactors;
+
+      Parabola floweringTempCurve;
+      double floralInductionUnitsRequired; // default 7
+      double floralInductionUnits;
+      bool dayNeutral;
+      bool longDayPlant; // long day/short night plant if true, short day / long night plant if false.
+      double minimumInduction;
+      double criticalNightLength;
+      SCurve nightLengthCurve;
+      static SCurve getSCurve(const bool dayNeutral, const bool longDayPlant, double minInduction, const double& optimalInductionNightLength);
+
       double biomassToVPD; // bc(3)
-      double biomass; // kg
+      BiomassHolder Biomass;  // kg
+
       double nitrogen; // kg/ha
 
       bool isAnnual; // limits HU to the maturity HUs.
+      double flowerFactor; // % of flowering successfully done.
 
       SoilCell* soilPatch;
 
@@ -90,9 +100,15 @@ namespace ALMANAC
       double barometricPressure(const double& altitude);
       double getWaterStressFactor();
       double getWaterlogStressFactor();
-      double getNitrogenStressFactor();
+      double getNitrogenStressFactor(); 
+
+      double floweringHU;
+      double finalHU;
+      double maxHU;
 
       void doWater(const WeatherData& data);
       void doNitrogen();
+      void doFloralInduction(const WeatherData& data);
+      void partitionBiomass(const double dBiomass);
     };
   }
